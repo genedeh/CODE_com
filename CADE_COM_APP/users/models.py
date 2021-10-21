59f4c52548663164e9320830169bbd5dacde6ff9
KidChaos <genesis.enedeh@gmail.com>
2021-10-21 19:23:34 +0100

    Put in a new model for users

--- CADE_COM_APP/users/models.py
+++ CADE_COM_APP/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=120, unique=True)
+    password = models.CharField(max_length=8, unique=True)
+    email = models.EmailField(default='')
+    profile_image = models.ImageField(default="User_Image/no_profile_image.jpg", unique=True)
+    description = models.TextField(max_length=1000, blank=True, default='')
+
+    def __str__(self):
+        return f'{self.username}'
